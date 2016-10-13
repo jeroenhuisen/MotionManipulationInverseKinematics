8fd4da35d9f96ae12f00bf391e71ac289e307499
JeroenHuisen <jeroenhuisen@hotmail.com>
2016-10-13 23:31:49 +0200

    Test armadillo works

--- MotionManipulation/Main.cpp
+++ MotionManipulation/Main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
 
 void main() {
-	std::cout << "welcome noob";
+	std::cout << "welcome noob" << std::endl;
+	mat a = randu<mat>(4, 5);
+	mat b = randu<mat>(4, 5);
+
+	
+	cout << a*b.t() << endl;
+
 	system("pause");
 }
